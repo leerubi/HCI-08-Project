f12ad8dd500deb764b9e286e9672a9450c8304f1
leerubi <dj112200@gmail.com>
2018-11-26 00:26:46 +0900

    init SecondButton project

---
+++ SecondButton/app/src/main/java/com/team8/hci/secondbutton/ApplicationListActivity.kt
@@ -0,0 +1,12 @@
+package com.team8.hci.secondbutton
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplicationListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application_list)
+    }
+}

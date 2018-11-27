4f62bc7b9acbc1b6c03dd6843429fde087d032a6
leerubi <dj112200@gmail.com>
2018-11-28 00:01:53 +0900

    성빈아 화이팅 ^^

--- LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/ApplicationListActivity.kt
+++ LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/ApplicationListActivity.kt
@@ -4,11 +4,6 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.EditText
-import android.R.id.button1
-import android.view.View
-import android.widget.Switch
-import kotlinx.android.synthetic.main.activity_application_list.*
 import android.support.v4.app.NotificationManagerCompat
 
 

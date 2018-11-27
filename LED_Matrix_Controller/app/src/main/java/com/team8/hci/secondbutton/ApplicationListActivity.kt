2a1044355bcd2b40a0391d486236f95e2856a8d8
leerubi <dj112200@gmail.com>
2018-11-27 21:51:44 +0900

    implementing switching activities using intent (KeywordAlarmActivity의 state 저장 못하는 상태) change app_name to LED Matrix Controlloer

--- LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/ApplicationListActivity.kt
+++ LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/ApplicationListActivity.kt
@@ -1,12 +1,37 @@
 package com.team8.hci.secondbutton
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ApplicationListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application_list)
+
+
+        // 키워드 알림 텍스트를 누르면 KeywordAlarmActivity로 전환.
+        val keywordName1 = findViewById(R.id.keywordName1) as TextView
+
+        keywordName1.setOnClickListener {
+            val i = Intent(this@ApplicationListActivity, KeywordAlarmActivity::class.java)
+            startActivity(i)
+//            finish()
+        }
+
+        val keywordOnOffText1 = findViewById(R.id.keywordOnOffText1) as TextView
+
+        keywordOnOffText1.setOnClickListener {
+
+        }
+
+        val keywordOnOffText2 = findViewById(R.id.keywordOnOffText2) as TextView
+
+        keywordOnOffText2.setOnClickListener {
+
+        }
+
     }
 }

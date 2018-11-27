4edc566fc5415fab602638b229fa71b47fd15e08
leerubi <dj112200@gmail.com>
2018-11-27 23:18:27 +0900

    implement NotificationPermission

--- LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/ApplicationListActivity.kt
+++ LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/ApplicationListActivity.kt
@@ -4,6 +4,15 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.EditText
+import android.R.id.button1
+import android.view.View
+import android.widget.Switch
+import kotlinx.android.synthetic.main.activity_application_list.*
+import android.support.v4.app.NotificationManagerCompat
+
+
+
 
 class ApplicationListActivity : AppCompatActivity() {
 
@@ -11,27 +20,51 @@ class ApplicationListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application_list)
 
+        // 알람 정보 퍼미션 받아오기.
+        val isPermissionAllowed = isNotiPermissionAllowed()
+        if (!isPermissionAllowed) {
+            val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            startActivity(intent)
+        }
+
 
         // 키워드 알림 텍스트를 누르면 KeywordAlarmActivity로 전환.
         val keywordName1 = findViewById(R.id.keywordName1) as TextView
-
         keywordName1.setOnClickListener {
-            val i = Intent(this@ApplicationListActivity, KeywordAlarmActivity::class.java)
-            startActivity(i)
-//            finish()
+            startKeywordAlarmActivity()
         }
 
         val keywordOnOffText1 = findViewById(R.id.keywordOnOffText1) as TextView
-
         keywordOnOffText1.setOnClickListener {
-
+            startKeywordAlarmActivity()
         }
 
         val keywordOnOffText2 = findViewById(R.id.keywordOnOffText2) as TextView
-
         keywordOnOffText2.setOnClickListener {
-
+            startKeywordAlarmActivity()
         }
 
     }
+
+    private fun isNotiPermissionAllowed(): Boolean {
+        val notiListenerSet = NotificationManagerCompat.getEnabledListenerPackages(this)
+        val myPackageName = packageName
+
+        for (packageName in notiListenerSet) {
+            if (packageName == null) {
+                continue
+            }
+            if (packageName == myPackageName) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    fun startKeywordAlarmActivity() {
+        val intent = Intent(this@ApplicationListActivity, KeywordAlarmActivity::class.java)
+        startActivity(intent)
+    }
+
 }
--- LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/KeywordAlarmActivity.kt
+++ LED_Matrix_Controller/app/src/main/java/com/team8/hci/secondbutton/KeywordAlarmActivity.kt
@@ -1,12 +1,54 @@
 package com.team8.hci.secondbutton
 
+import android.app.Notification
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_application_list.*
+import kotlinx.android.synthetic.main.activity_keyword_alarm.*
+import android.content.Intent
+import android.service.notification.StatusBarNotification
+import android.graphics.Bitmap
+import android.util.Log
+
 
 class KeywordAlarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keyword_alarm)
+
+        keywordSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this, "Switch on.", Toast.LENGTH_LONG).show()
+
+            } else {
+                Toast.makeText(this, "Switch Off.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
+    fun onNotificationPosted(sbn: StatusBarNotification) {
+        Log.i("NotificationListener", "[snowdeer] onNotificationPosted() - " + sbn.toString())
+        Log.i("NotificationListener", "[snowdeer] PackageName:" + sbn.packageName)
+        Log.i("NotificationListener", "[snowdeer] PostTime:" + sbn.postTime)
+
+        val notificatin = sbn.notification
+        val extras = notificatin.extras
+        val title = extras.getString(Notification.EXTRA_TITLE)
+        val smallIconRes = extras.getInt(Notification.EXTRA_SMALL_ICON)
+        val largeIcon = extras.getParcelable(Notification.EXTRA_LARGE_ICON) as Bitmap
+        val text = extras.getCharSequence(Notification.EXTRA_TEXT)
+        val subText = extras.getCharSequence(Notification.EXTRA_SUB_TEXT)
+
+        Log.i("NotificationListener", "[snowdeer] Title:" + title!!)
+        Log.i("NotificationListener", "[snowdeer] Text:" + text!!)
+        Log.i("NotificationListener", "[snowdeer] Sub Text:" + subText!!)
+    }
+
+    fun onNotificationRemoved(sbn: StatusBarNotification) {
+        Log.i("NotificationListener", "[snowdeer] onNotificationRemoved() - " + sbn.toString())
+    }
+
+
 }
